34139bf116577b82fc8543454b0aa59cdd1f356b
lisandro <lisandrogabrielmiranda@gmail.com>
2025-07-21 01:10:28 -0300

    lisan

---
+++ main.js
@@ -0,0 +1,9 @@
+const edad = Number(prompt("¿Cuál es tu edad?"));
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+    alert("¡Bienvenido!");
+}else {
+    console.log("Eres menor de edad");
+    alert("Eres menor de edad");
+}
